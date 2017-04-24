b7ec94f4c3bff3d74463d495b52bbd499d1943fc
Jessie Wang <jessiewangyuji@Jessies-MacBook-Pro.local>
2017-04-24 02:23:49 -0400

    sshd intrusion

--- A5/audit.py
+++ A5/audit.py
@@ -1,6 +1,9 @@
 import sys
 from datetime import datetime
+from sets import Set
 TIME_WINDOW = 600
+SSH_TIME_WINDOW = 60 * 120
+SSH_NUM_IP = 1
 LOGIN_ATTEMPT = 10
 LOGIN_ATTEMPT_ALREADY_LOGEDIN = 3
 
@@ -8,11 +11,15 @@ logpath = sys.argv[1]
 
 failure_time = {}
 user_attack = []
+ssh_user_attack = {}
 login = []
+ssh_ip_access_time = {}
+ssh_ip_access_user = {}
 now = datetime.now()
 curryear = now.year
 with open(logpath, "r") as log:
     for line in log:
+        now = datetime.now()
         if "authentication failure" in line:
             time = line.split(" ")[0:3]
             user = line.split(" ")[-1] #TODO:user + uid?
@@ -42,22 +49,71 @@ with open(logpath, "r") as log:
                 list = []
                 list.append(datetime_obj)
                 failure_time[user] = list
-        elif "ssh" in line:
-            print "ssh"
-            #TODO: ssh
-        elif "session closed" in line:
+        elif "sshd" in line:
+            time = line.split(" ")[0:3]
+            time = ' '.join(time)
+            datetime_obj = datetime.strptime(time, "%b %d %X")
+            datetime_obj = datetime_obj.replace(year = curryear)
+            if "maximum authentication attempts exceeded" in line:
+                line_split = line.split(" ")
+                ip = line_split[line_split.index("port") - 1]
+                if ip in ssh_user_attack:
+                    ssh_user_attack[user].add(user)
+                else:
+                    userset = Set([user])
+                    ssh_user_attack[ip] = userset
+                print "max", ssh_user_attack, "..........."
+            elif "Failed password" in line:
+                print "failed password"
+                    #report intrusion
+            elif "port" in line and "password" in line:
+                line_split = line.split(" ")
+                ip = line_split[line_split.index("port") - 1]
+                username = line_split[line_split.index("from") - 1]
+                if ip in ssh_ip_access_time:
+                    timelist = ssh_ip_access_time[ip]
+                    userlist = ssh_ip_access_user[ip]
+                    for i in range(0, len(timelist) - 1):
+                        if (datetime_obj - timelist[i]).total_seconds > SSH_TIME_WINDOW:
+                            del timelist[i]
+                            del userlist[i]
+                        else:
+                            break
+                else:
+                    timelist = []
+                    userlist = []
+                timelist.append(datetime_obj)
+                userlist.append(username)
+                if len(set(userlist)) > SSH_NUM_IP:
+                    ssh_user_attack[ip] = set(userlist)
+                ssh_ip_access_time[ip] = timelist
+                ssh_ip_access_user[ip] = userlist
+                print ssh_ip_access_user
+        elif "session closed" in line and "sshd" not in line:
             user = line.split(" ")[-1]
+            if user in login:
+                del login[login.index(user)]
+        elif "session closed" in line and "sshd" in line:
+            line_split = line.split(" ")
+            user = line_split[line_split.index("user") + 1]
             if user in login:
                 del login[login.index(user)]
         elif "New session" in line:
             user = line.split(" ")[-1]
             if user not in login:
                 login.append(user)
+        elif "session opened" in line:
+            line_split = line.split(" ")
+            user = line_split[line_split.index("user") + 1]
+            login.append(user)
 
-
-if len(user_attack) == 0:
+if len(user_attack) == 0 and len(ssh_user_attack) == 0:
     print "OK" #if login is successful
 else:
     print "Intrusion" #detect intrusion
-    print (str(len(user_attack))+' users might be under attack')
+    num = 0
+    for i in ssh_user_attack:
+        num += len(ssh_user_attack[i])
+    num += len(user_attack)
+    print (str(num)+' users might be under attack')
 #further estimate the number of users whose accounts the attacker attempted to access
